4e5ce33ae4aa1b0939ee96c553792e8086d7815d
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-06-27 15:36:06 +0800

    release: 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.muedsa.tvbox.agetv"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.2.0"
+        versionCode = 14
+        versionName = "0.2.1"
     }
 
     signingConfigs {
